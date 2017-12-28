9bab17d59260811fe09e8eba5f9bb925230224c9
hbinh <hbinh@yahoo-corp.jp>
2017-12-28 16:44:57 +0700

    First commit

---
+++ image-list.js
@@ -0,0 +1,22 @@
+var Path = 'gallery/';
+
+var Gallery = { 'images' : [
+  {
+    'name'  : 'Darth Vader',
+    'alt' : 'A Black Clad warrior lego toy',
+    'url': 'gallery/myLittleVader.jpg',
+  },
+
+  {
+    'name'  : 'Snow Troopers',
+    'alt' : 'Two lego solders in white outfits walking across an icy plain',
+    'url': 'gallery/snowTroopers.jpg',
+  },
+
+  {
+    'name'  : 'Bounty Hunters',
+    'alt' : 'A group of bounty hunters meeting, aliens and humans in costumes.',
+    'url': 'gallery/bountyHunters.jpg',
+  },
+  
+]};
